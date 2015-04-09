a07b79c7b3ac48702c017bad161003871bd70b26
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-04-09 19:47:35 +0300

    Added systemd support
    This adds a dependency to puppetfinland/systemd and tightens the integration
    with puppetfinland/os.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,14 +15,6 @@ class postgresql::params {
             $pidfile = '/var/run/postmaster.5432.pid'
             $service_name = 'postgresql'
             $daemon_user = 'postgres'
-
-            if $::operatingsystem == 'Fedora' {
-                $service_start = "/usr/bin/systemctl start ${service_name}.service"
-                $service_stop = "/usr/bin/systemctl stop ${service_name}.service"
-            } else {
-                $service_start = "/sbin/service $service_name start"
-                $service_stop = "/sbin/service $service_name stop"
-            }
         }
 
         # ...and Debian/Ubuntu do. Therefore we need to accurately detect the 
@@ -31,6 +23,7 @@ class postgresql::params {
         # node was upgraded) then this logic will fail.
         'Debian': {
             case $::lsbdistcodename {
+                'jessie':            { $ver = 9.4 }
                 'trusty':            { $ver = 9.3 }
                 'wheezy', 'precise': { $ver = 9.1 }
                 'squeeze':           { $ver = 8.4 }
@@ -42,12 +35,18 @@ class postgresql::params {
             $pg_hba_conf = "${data_dir}/pg_hba.conf"
             $pidfile = "/var/run/postgresql/${ver}-main.pid"
             $service_name = 'postgresql'
-            $service_start = "/usr/sbin/service $service_name start"
-            $service_stop = "/usr/sbin/service $service_name stop"
             $daemon_user = 'postgres'
         }
         default: {
             fail("Unsupported operating system: ${::osfamily}/${::operatingsystem}")
         }
+    }
+
+    if $::has_systemd == 'true' {
+        $service_start = "${::os::params::systemctl} start ${service_name}"
+        $service_stop = "${::os::params::systemctl} stop ${service_name}"
+    } else {
+        $service_start = "${::os::params::service_cmd} ${service_name} start"
+        $service_stop = "${::os::params::service_cmd} ${service_name} stop"
     }
 }
